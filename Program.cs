8ad520d064a74e635480b4cc3be738a3d9f5c90a
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-07-15 22:50:15 -0300

    Aprendendo sobre a estrutura do .net

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics.Contracts;
+namespace MeuApp
+{
+    class Program
+
+    {
+        static void Main(string[] args) 
+        {
+            Product mouse = new Product(1, "Mouse Gamer", 350.00);
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+        }
+    }
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }  
+    }
+}
--- obj/Release/net8.0/MeuApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MeuApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ecbeb7419dab1119568c8bc73cc557e028c6ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2a747184ee4aebc0cab01d9897897044db0dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
