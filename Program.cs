2dd27c6713023361ebd17fd2561a0e45b10899f4
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-07-15 22:59:11 -0300

    Criando struct em um codigo

--- Program.cs
+++ Program.cs
@@ -7,27 +7,37 @@ namespace MeuApp
     {
         static void Main(string[] args) 
         {
-            Product mouse = new Product(1, "Mouse Gamer", 350.00);
+            Product mouse = new Product(1, "Mouse Gamer", 350.00, EProductType.Product);
+            var manutencaoEletrica = new Product(2,"Manutenção Predial",300.00, EProductType.Service);
             Console.WriteLine(mouse.Id);
             Console.WriteLine(mouse.Name);
             Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine((int)mouse.Type);
         }
     }
     struct Product
     {
-        public Product(int id, string name, double price)
+        public Product(int id, string name, double price, EProductType type)
         {
             Id = id;
             Name = name;
             Price = price;
+            Type = type;
         }
         public int Id;
         public string Name;
         public double Price;
+        public EProductType Type;
 
         public double PriceInDolar(double dolar)
         {
             return Price * dolar;
         }  
+    }
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
     }
 }
